52e33bd2ddcdf1b6065b2dfcf19982d328217cc2
Helena Antoni <helenaantoni03@gmail.com>
2024-09-10 21:07:45 -0300

    add server

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,9 +1,32 @@
-import fastify from "fastify";
+import fastify from 'fastify'
+import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod";
+import { createGoal } from '../functions/create-goal'
+import z from 'zod'
 
 const app = fastify()
 
-app.listen({
-   port: 3333, 
-}).then(() => {
-   console.log('HTTP server running!')
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+app.post('/goals', async (request) => {
+
+   const createGoalSchema = z.object({
+      title: z.string(),
+      desiredWeeklyFrequency: z.number().int().min(1).max(7),
+  })
+
+  const body = createGoalSchema.parse(request.body)
+
+   await createGoal({
+      title: body.title,
+      desiredWeeklyFrequency: body.desiredWeeklyFrequency,
+  })
 })
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('HTTP server running!')
+  })
